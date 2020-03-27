a99ef578e7f32a99aded505654a467be8fba0bc7
Chris Larsen <clarsendev@protonmail.com>
2020-03-27 16:25:55 -0400

    Fix tests

--- test/covidapi_test.cr
+++ test/covidapi_test.cr
@@ -6,11 +6,13 @@ require "./webmocks.cr"
 class CountryHistoryTest < Minitest::Test
   def test_gets_country_history
     canada = Covidapi.getCountryHistory("CAN")
-    assert_equal canada.count, canada.results.size
+    assert_equal 63, canada.results.size
+    assert_equal 2790, canada.results.last_value.confirmed
   end
 
   def test_gets_global_history
     global = Covidapi.getGlobalHistory
-    assert_equal global.count, global.results.size
+    assert_equal 63, global.results.size
+    assert_equal 418678, global.results.last_value.confirmed
   end
 end
