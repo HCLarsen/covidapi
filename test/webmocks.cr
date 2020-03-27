78d285831814897f254c3168050845c7960128e6
Chris Larsen <clarsendev@protonmail.com>
2020-03-26 22:09:47 -0400

    Add more webmocks

--- test/webmocks.cr
+++ test/webmocks.cr
@@ -1,7 +1,17 @@
 require "webmock"
 
-file = File.new("test/files/canada.json")
-canada_history = file.gets_to_end
-file.close
+def loadfile(filename : String)
+  file = File.new("test/files/#{filename}.json")
+  content = file.gets_to_end
+  file.close
 
-WebMock.stub(:get, "https://covidapi.info/api/v1/country/CAN").to_return(status: 200, body: canada_history)
+  return content
+end
+
+WebMock.stub(:get, "https://covidapi.info/api/v1/country/CAN").to_return(status: 200, body: loadfile("canada"))
+
+WebMock.stub(:get, "https://covidapi.info/api/v1/country/CAN/latest").to_return(status: 200, body: loadfile("canada-latest"))
+
+WebMock.stub(:get, "https://covidapi.info/api/v1/global").to_return(status: 200, body: loadfile("global-latest"))
+
+WebMock.stub(:get, "https://covidapi.info/api/v1/global/count").to_return(status: 200, body: loadfile("global"))
